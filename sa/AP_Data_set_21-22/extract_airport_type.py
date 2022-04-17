206782e4a15513c7df2577d3272e7c23b302d7fe
Joel Nash <joelnash@xuuki.xyz>
2022-04-17 20:34:48 +0100

    create cols using airprt type

--- sa/AP_Data_set_21-22/extract_airport_type.py
+++ sa/AP_Data_set_21-22/extract_airport_type.py
@@ -2,9 +2,15 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('airports.csv', delimiter = ',')
+df2 = pd.read_csv('airport-frequencies.csv', delimiter = ',')
+
 df['Large Airport'] = np.where(df['type'] == 'small_airport', True, False)
 df['Medium Airport'] = np.where(df['type'] == 'medium_airport', True, False)
 df['Small Airport'] = np.where(df['type'] == 'small_airport', True, False)
-print(df.head())
+df.drop(df.index[df['type'] == 'closed'], inplace=True)
+
+if (np.where(df['type'] == df2['type'], True, False)) == True:
+
+#print(df.head())
 
 
