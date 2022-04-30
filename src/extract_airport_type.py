0fec2d6863ec84f2d4f003d207b11273cef8ef63
Joel Nash <joelnash@xuuki.xyz>
2022-04-30 16:21:24 +0100

    update

--- src/extract_airport_type.py
+++ src/extract_airport_type.py
@@ -3,27 +3,41 @@ import numpy as np
 
 print("Programe start")
 
+#Load data from files 
 data_1 = pd.read_csv('../data/AP_Data/airports.csv', delimiter = ',')
 df1 = pd.DataFrame(data_1)
-
 data_2 = pd.read_csv('../data/AP_Data/airport-frequencies.csv', delimiter = ',')
 df2 = pd.DataFrame(data_2)
 
+#Extract each 'type of airport into its own column
 df1['Large Airport'] = np.where(df1['type'] == 'small_airport', True, False)
 df1['Medium Airport'] = np.where(df1['type'] == 'medium_airport', True, False)
 df1['Small Airport'] = np.where(df1['type'] == 'small_airport', True, False)
 
+#Remove closed airports
 df1.drop(df1.index[df1['type'] == 'closed'], inplace=True)
-df1 = df1.reset_index()  # make sure indexes pair with number of rows
 
-for index, row in df1.iterrows():
-  airportID= row['id']
-  for index, row in df2.iterrows():
-    airportFreqRow= row['id2']
-    if airportID == airportFreqRow: 
-      print("Test")
-      df1['Com Freq']= row['frequency_mhz']
-      exit()
+# Match each airport in all catagories with its communication freq
+
+communicationFreq= []
+
+for index, row in df2.iterrows():
+  idWithComFreq= df2.iloc[index][1]
+  for index, row in df1.iterrows(): 
+    idToMatchIdWithComFreq= idWithComFreq= df1.iloc[index][0]
+    if idWithComFreq == idToMatchIdWithComFreq:
+      communicationFreq.append(df2.iloc[index])
+  
+  
+
+#for index, row in df1.iterrows():
+#  airportID= row['id']
+#  for index, row in df2.iterrows():
+#    airportFreqRow= row['id2']
+#    if airportID == airportFreqRow: 
+#      print("Test")
+#      df1['Com Freq']= row['frequency_mhz']
+#      exit()
 
 print("Programe end")
 
